2bbaebadd14e54f4d1885d980accb68261ad8cd2
qshaner <quinn.shaner@gmail.com>
2020-10-21 17:29:10 -0400

    uses the app registration as default

--- app/src/main/java/com/example/covid19notification/ui/accountregistration/AccountRegistrationFragment.kt
+++ app/src/main/java/com/example/covid19notification/ui/accountregistration/AccountRegistrationFragment.kt
@@ -3,12 +3,15 @@ package com.example.covid19notification.ui.accountregistration
 import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.covid19notification.MainActivity
 import com.example.covid19notification.R
 import com.example.covid19notification.ui.Contact.contactActivtiy
 import com.example.covid19notification.ui.ui.SymptomTracker.SymptomTracker
@@ -53,11 +56,23 @@ class AccountRegistrationFragment  : Fragment(), View.OnClickListener {
         val email = mEtEmail.text.toString()
         val address = mEtAddress.text.toString()
         val activity = requireActivity()
+        val tag = "AccountRegistration"
 
         //make sure that user, password, and email are non-empty
         //address can be empty
+        if(username != "" && password != "" && email !=""){
+            //TODO: Hook in database here
+            //Check against database
+            //if check succeeds, create account and go to login page
+            startActivity(Intent(activity.applicationContext, MainActivity::class.java))
+            //TODO: Change this to be the login page instead of the main activity
 
-        //if empty, show error
+            //if check fails, show error
+        } else if (username == "" || password == "" || email == "") {
+            Toast.makeText(activity.applicationContext, "Missing entry", Toast.LENGTH_SHORT).show()
+        }else {
+            Log.d(tag, "There was an error in creating the account")
+        }
     }
 
 }
