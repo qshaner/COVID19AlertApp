6bc41627d12da8d3231217ab73d9b948d7ee5f3f
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2020-12-13 16:20:16 -0500

    first commit

---
+++ app/src/androidTest/java/com/example/covid19notification/ui/accountregistration/AccountRegistrationTest.kt
@@ -0,0 +1,74 @@
+package com.example.covid19notification.ui.accountregistration
+
+
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+
+import androidx.test.InstrumentationRegistry.getInstrumentation
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+import com.example.covid19notification.R
+
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.`is`
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class AccountRegistrationTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(AccountRegistration::class.java)
+
+    @Test
+    fun accountRegistrationTest() {
+        val appCompatButton = onView(
+allOf(withId(R.id.buttonLogin), withText("Log in"),
+childAtPosition(
+allOf(withId(R.id.account_registration),
+childAtPosition(
+withId(R.id.fragment_container),
+0)),
+7),
+isDisplayed()))
+        appCompatButton.perform(click())
+        }
+    
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+    }
